0503169b72d0ac6cf0aff4a803565e6262e79e44
Irfan Ali <alimohammadirfan7@gmail.com>
2023-05-18 21:54:05 +0600

    basic route setup

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (reg, res) => {
+    res.send("animal server server is running");
+})
+
+
+app.listen(port, () => {
+    console.log("app running");
+})
